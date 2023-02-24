90aa0bed9f367c8ad13f9632ce3c2db7cad25380
tHeStRyNg <info@algorithmic.one>
2023-02-24 20:03:28 +0100

    Initial Version

---
+++ README.md
@@ -0,0 +1,3 @@
+### Silent Install Automation for IBM Connect Direct 6.2.0 version
+
+
---
+++ spcust_control.sh
@@ -0,0 +1,151 @@
+#! /bin/sh
+#
+#############################################################################
+# Licensed Materials - Property of IBM
+#
+# Connect:Direct for UNIX
+#
+# (C) Copyright IBM Corp. 1992, 2016 All Rights Reserved.
+#
+# US Government Users Restricted Rights - Use, duplication or disclosure
+# restricted by GSA ADP Schedule Contract with IBM Corp.
+#############################################################################
+
+# spcust_sample1.sh contains an example of configuring
+# Secure+ to use SSL or TLS protocols with the Secure+ CLI. 
+# The example demonstrates the configuration of Secure+
+# with the trusted root and key certificates and ciphers
+#
+
+
+#
+# Variables
+#
+
+# The return code. 
+# spcli.sh returns the highest return code of the commands
+# it executed. Possible return codes and their meanings are
+#      0    success
+#      4    warning
+#      8    error
+#     16    fatal error
+RC=0
+
+
+#
+# Functions
+#
+
+#
+# Custom initialization logic written by customer.
+#
+initCustom()
+{
+    # Customer adds custom initialization code here.
+    echo "Init custom..."
+    # rm -rf /home/control/cdunix/ndm/secure+/nodes
+}
+
+#
+# Invoke CLI to configure Secure+.
+#
+invokeCLI()
+{
+    /home/control/cdunix/ndm/bin/spcli.sh -e 8 -li y << EOF
+    ;
+    display info
+    ;
+    ;
+    ; -- Synch with netmap
+    ;
+    sync netmap 
+        path=/home/control/cdunix/ndm/cfg/control/netmap.cfg
+        name=*
+    ;
+    ;
+    ; -- Import KeyCert
+    ;
+    Import KeyCert
+        File=/home/control/cdunix/ndm/secure+/certificates/keycert
+        Passphrase=P@ssw0rd
+        Label=control
+        ImportMode=Add
+    ;
+    ;
+    ; -- Import TrustedCert
+    ;
+    Import TrustedCert
+        File=/home/control/cdunix/ndm/secure+/certificates/cert
+        ImportMode=Add
+    ;
+    ;
+    ; -- Update LocalNode
+    ;
+    Update LocalNode
+        Protocol=TLS1.2
+        Override=n
+        AuthTimeout=120
+        KeyCertLabel=control
+        EncryptData=y
+        ClientAuth=n
+        CipherSuites=All
+        SeaEnable=n
+    ; -- Update RemoteNode
+    ;
+    Update RemoteNode
+	name=ansible1
+        Protocol=TLS1.2
+        Override=n
+        AuthTimeout=120
+        KeyCertLabel=control
+        EncryptData=y
+        ClientAuth=n
+        CipherSuites=All
+        SeaEnable=n
+    ;
+    ;
+    ; -- Display localnode
+    ;
+    display localnode
+    ; 
+    ;
+    ; -- Validate parmfile
+    ;
+    validate parmfile
+    ;
+EOF
+
+    return $?
+}
+
+#
+# Custom termination logic written by customer.
+#
+terminateCustom()
+{
+    # Customer adds custom termination code here.
+    # For example, E-mail standard out log for review.
+    # Send error messages to system monitoring facility. 
+    echo "$RC"
+    echo "Custom Terminating ... "
+}
+
+
+#
+#  Main script
+#
+
+echo
+echo "This script has been prevented from running because it will alter the configuration"
+echo "of Secure+. Before removing this warning and its exit call, please modify the script"
+echo "so that it carries out only desired modifications to the configuration of Secure+." 
+echo  
+
+initCustom
+
+invokeCLI
+RC=$?
+
+terminateCustom
+
+exit $RC 
